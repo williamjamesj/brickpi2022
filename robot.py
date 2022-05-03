34e3f44e00735dc7fdbf0be9657041853d9720da
Will Johnson <22johnsonw@marash.qld.edu.au>
2022-05-03 19:31:21 +1000

    a lot of really bad code

--- robot.py
+++ robot.py
@@ -1,5 +1,6 @@
 #This is where your main robot code resides. It extendeds from the BrickPi Interface File
 #It includes all the code inside brickpiinterface. The CurrentCommand and CurrentRoutine are important because they can keep track of robot functions and commands. Remember Flask is using Threading (e.g. more than once process which can confuse the robot)
+from ast import Return
 from interfaces.brickpiinterface import *
 # from flask_app import logaction
 import global_vars as GLOBALS
@@ -61,7 +62,6 @@ class Robot(BrickPiInterface):
         self.interrupt_previous_command()
         self.CurrentCommand = "move_square"
         start_time = time.time()
-        print(start_time)
         end_time = start_time+traveltime
         ultra = self.get_ultra_sensor()
         if power+error > 100:
@@ -72,7 +72,6 @@ class Robot(BrickPiInterface):
             self.BP.set_motor_power(self.leftmotor, power+error*powermodifier)
         while time.time() < end_time and self.CurrentCommand == "move_square" and ultra >= 10:
             ultra = self.get_ultra_sensor()
-            print(ultra)
         print(ultra)
         self.stop_all()
         return
@@ -84,7 +83,6 @@ class Robot(BrickPiInterface):
         self.x = 0
         self.y = 0
         self.orientation = 0 # 0 is right, 1 is left, 2 is backwards, 3 is forwards
-        print(self.directions)
         while True:
             for i in self.directions: # Search all of the four directions that a wall/victim can be.
                 distance = ROBOT.get_ultra_sensor()
@@ -96,47 +94,60 @@ class Robot(BrickPiInterface):
                     self.maze_progress[str([self.x,self.y,self.directions.index(i)])] = False
                 print("turning to ",i)
                 ROBOT.turn(i,25) # Turn to the necessary direction using the orientation-based turning function
+
+            # These variables indicate if there is a wall in any four directions (relative to the robot's start)
+            # There is a wall present if the value is False
             forward = self.maze_progress[str([self.x,self.y,0])]
             right = self.maze_progress[str([self.x,self.y,1])]
             backward = self.maze_progress[str([self.x,self.y,2])]
             left = self.maze_progress[str([self.x,self.y,3])]
-            print(forward,right,backward,left)
-            print(self.maze_progress)
-            if left:
-                self.orientation =  2
-                self.turn(self.directions[2],10)
-                self.move_square(1,2.5)
-                self.x -= 1
-            elif right:
-                self.orientation =  1
-                self.turn(self.directions[0],10)
-                self.move_square(1,2.5)
-                self.x += 1
-            elif forward:
-                self.orientation =  0
-                self.turn(self.directions[3],10)
-                self.move_square(1,2.5)
-                self.y += 1
+            directions = {0:right,1:backward,2:left,3:forward} # I'm not sure how it got to this, but this is undoubtedly the wrong thing to do.
+            newdirections = {} # A new dictionary has to be created, as the original dictionary cannot have elements removed as it is iterated through.
+            for direction in directions: # This loop eliminates any direction that has walls.
+                if directions[direction]: # Remove direction from the dictionary, if there is a wall blocking the way.
+                    newdirections[direction] = directions[direction]
+            directions = newdirections
+            newdirections = {}
+            for direction in directions: # Checks if any of the other directions have already been visited.
+                if not self.remember_square([self.x,self.y,direction]):
+                    newdirections[direction] = directions[direction]
+            print(directions)
+            if len(directions) == 0: # If there are no directions left, then the robot has reached the end of the maze, or is completely stuck between four walls, and will try to drive straight through them.
+                break
+            # The directions list now just contains the directions that are not walls or haven't already been visited.
+            for index, direction in enumerate(self.directions): # This loop will iterate through the list of directions, and choose based on that priority.
+                print(index, direction)
+                if index in directions:
+                    self.turn(direction,20)
+                    self.move_square(1,2.5)
+                    if index == 0:
+                        self.y += 1
+                    elif index == 1:
+                        self.x += 1
+                    elif index == 2:
+                        self.y -= 1
+                    elif index == 3:
+                        self.x -= 1
+                    break
             else:
-                self.orientation =  3
-                self.turn(self.directions[1],10)
-                self.move_square(1,2.5)
-                self.y -= 1
+                break
+            break
+        self.return_to_start()
         return
-            
-    # def move_forward_encoder(self,distance):
-    #     speed =150 # Degrees per second
-    #     power = 100 # 100%
-    #     encoder_distance = distance*360/(3.14159265359 * 5.6)
-    #     BP = self.BP
-    #     BP.offset_motor_encoder(BP.PORT_A, BP.get_motor_encoder(BP.PORT_A))
-    #     BP.offset_motor_encoder(BP.PORT_A, BP.get_motor_encoder(BP.PORT_A))
-    #     BP.set_motor_limits(BP.PORT_A, power, speed)
-    #     BP.set_motor_limits(BP.PORT_D, power, speed)
-    #     while True:
-    #         print(( BP.get_motor_encoder[BP.PORT_A] %720 ) /2)   # print the encoder degrees 
-    #         time.sleep(.1)	
-    #Create a function to search for victim
+
+    def return_to_start(self):
+        print("Returning to start")
+        return
+
+    def remember_square(self, coordinate):
+        try: # If this next if statement returns an error, the square has not yet been visited, or not have the forward direction scanned (not sure why that would happen?).
+            if self.maze_progress[str(coordinate.append(0))]:
+                return True
+            else:
+                return False # Should never happen, as a KeyError should be raised.
+        except KeyError: # The key error means that the square has not yet been visited.
+            return False
+        return # Also should never reach this part.
     
     def turn(self, degrees, power):
         current_direction = self.get_orientation_IMU()[0]
@@ -163,7 +174,7 @@ class Robot(BrickPiInterface):
                 compass = self.get_orientation_IMU()[0]
                 # print(compass)
         print(compass)
-        pass
+        return
 
     
     
